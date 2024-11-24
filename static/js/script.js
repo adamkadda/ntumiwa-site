3bce46e01e665f1e168383b2de4a86187bcd086a
Adam Kadda <adamgkadda@gmail.com>
2024-11-24 22:49:54 +0100

    Create page header prototype
    Understanding the background-image property was a little tricky. It took
    me awhile to realize what box-sizing: border-box is for. I reorganized
    the project a little, specfically the static files.
    
    I learned that some of the things I did manually, like scaling images,
    is done automatically by Wordpress. Not that I care. Haha.
    
    I need to do a number of things, such as prepare another image, decide
    whether the blockquote placement can be improved, and make the hamburger
    bigger on larger devices (and its neighboring elements).

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector('#hamburger');
 const nav = document.querySelector('nav');
+const main = document.querySelector('main');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
     nav.classList.toggle('open');
+    main.classList.toggle('moved');
 
     document.body.classList.toggle("frozen");
 });
